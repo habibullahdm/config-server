21be5a2e9e01459120903337afbac745994aabd9
Habibullah Dzaky Musthafa <habibullahdm1@gmail.com>
2026-03-12 06:22:54 +0700

    update project configuration and rename packages

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,8 +4,7 @@ plugins {
     id("io.spring.dependency-management") version "1.1.4"
 }
 
-group = "com.learn"
-version = "0.0.1-SNAPSHOT"
+group = "com.habibullahdm"
 
 java {
     sourceCompatibility = JavaVersion.VERSION_17
--- src/test/java/com/habibullahdm/configserver/ConfigServerApplicationTests.java
+++ src/test/java/com/habibullahdm/configserver/ConfigServerApplicationTests.java
@@ -1,4 +1,4 @@
-package com.learn.configserver;
+package com.habibullahdm.configserver;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
